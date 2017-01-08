8ade265fc0f541602d5dbc55d911ae6a3824e191
conorsherman <conor@sherman.org>
2017-01-08 10:32:54 -0500

    Added the Process Name in the output file

--- hash_upload.py
+++ hash_upload.py
@@ -17,8 +17,13 @@ input_row = 2
 
 # Temp Dictionary to hold the results
 hash_report = {}
+# Temo Dictionary to hold the process name
+hash_process_name = {}
 
 for hash in range(1, input_sheet.max_row): #Needs to Start at 1 due to the headers
+    #Populating the HASH and process name
+    hash_process_name.update({input_sheet.cell(row=input_row, column=2).value : input_sheet.cell(row=input_row, column=1).value})
+    print hash_process_name
 
     #Get Value of the Cell
     hash_value=input_sheet.cell(row=input_row, column=2).value
@@ -49,18 +54,22 @@ for hash in range(1, input_sheet.max_row): #Needs to Start at 1 due to the heade
 # Open the Files to Write To Excel
 output_workbook = xlsxwriter.Workbook('/Users/conorsherman/Desktop/Tanium_Hash_Output.xlsx')
 output_worksheet = output_workbook.add_worksheet()
+name_col = 0
+hash_col = 1
+result_col = 2
 output_row = 0
-output_col = 0
 
 #Write Headers
-output_worksheet.write(output_row, output_col, "Process HASH")
-output_worksheet.write(output_row, output_col + 1, "VirusTotal Results")
+output_worksheet.write(output_row, name_col, "Process Name")
+output_worksheet.write(output_row, hash_col, "Process HASH")
+output_worksheet.write(output_row, result_col, "VirusTotal Results")
 output_row +=1 #Move to the next row
 
 #Grab the results from the Dictionary
 for key, value in hash_report.items():
-    output_worksheet.write(output_row, output_col, key) #Write the Hash
-    output_worksheet.write(output_row, output_col + 1, hash_report[key]) #Write the Result
+    output_worksheet.write(output_row, name_col, hash_process_name[key] )
+    output_worksheet.write(output_row, hash_col, key) #Write the Hash
+    output_worksheet.write(output_row, result_col, hash_report[key]) #Write the Result
     #Position for next Output
     output_row += 1
 
